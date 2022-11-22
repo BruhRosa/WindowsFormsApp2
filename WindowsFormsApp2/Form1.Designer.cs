069e28fa1e52bbcbde08a5d03a51562769c3bd21
curso.ads2 <curso.ads2@LAB06-63>
2022-11-21 22:02:14 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtagencia = new System.Windows.Forms.TextBox();
+            this.txtitular = new System.Windows.Forms.TextBox();
+            this.txtconta = new System.Windows.Forms.TextBox();
+            this.txtsaldo = new System.Windows.Forms.TextBox();
+            this.btncriar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            this.btnsacar = new System.Windows.Forms.Button();
+            this.btndepositar = new System.Windows.Forms.Button();
+            this.btnexibir = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Titular:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Agência:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(61, 340);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nº Conta:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(61, 432);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Saldo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Perpetua Titling MT", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(74, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(198, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ABRIR CONTA";
+            // 
+            // txtagencia
+            // 
+            this.txtagencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtagencia.Location = new System.Drawing.Point(175, 242);
+            this.txtagencia.Name = "txtagencia";
+            this.txtagencia.Size = new System.Drawing.Size(285, 31);
+            this.txtagencia.TabIndex = 5;
+            // 
+            // txtitular
+            // 
+            this.txtitular.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtitular.Location = new System.Drawing.Point(175, 155);
+            this.txtitular.Name = "txtitular";
+            this.txtitular.Size = new System.Drawing.Size(285, 31);
+            this.txtitular.TabIndex = 6;
+            // 
+            // txtconta
+            // 
+            this.txtconta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtconta.Location = new System.Drawing.Point(175, 337);
+            this.txtconta.Name = "txtconta";
+            this.txtconta.Size = new System.Drawing.Size(285, 31);
+            this.txtconta.TabIndex = 7;
+            // 
+            // txtsaldo
+            // 
+            this.txtsaldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsaldo.Location = new System.Drawing.Point(175, 426);
+            this.txtsaldo.Name = "txtsaldo";
+            this.txtsaldo.Size = new System.Drawing.Size(285, 31);
+            this.txtsaldo.TabIndex = 8;
+            // 
+            // btncriar
+            // 
+            this.btncriar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncriar.Location = new System.Drawing.Point(220, 496);
+            this.btncriar.Name = "btncriar";
+            this.btncriar.Size = new System.Drawing.Size(120, 43);
+            this.btncriar.TabIndex = 9;
+            this.btncriar.Text = "CRIAR";
+            this.btncriar.UseVisualStyleBackColor = true;
+            this.btncriar.Click += new System.EventHandler(this.btncriar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btnexibir);
+            this.panel1.Controls.Add(this.btndepositar);
+            this.panel1.Controls.Add(this.btnsacar);
+            this.panel1.Controls.Add(this.txtvalor);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(488, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 480);
+            this.panel1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(23, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Valor";
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtvalor.Location = new System.Drawing.Point(100, 59);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(144, 31);
+            this.txtvalor.TabIndex = 12;
+            // 
+            // btnsacar
+            // 
+            this.btnsacar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsacar.Location = new System.Drawing.Point(73, 288);
+            this.btnsacar.Name = "btnsacar";
+            this.btnsacar.Size = new System.Drawing.Size(120, 43);
+            this.btnsacar.TabIndex = 13;
+            this.btnsacar.Text = "Sacar";
+            this.btnsacar.UseVisualStyleBackColor = true;
+            this.btnsacar.Click += new System.EventHandler(this.btnsacar_Click);
+            // 
+            // btndepositar
+            // 
+            this.btndepositar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndepositar.Location = new System.Drawing.Point(73, 219);
+            this.btndepositar.Name = "btndepositar";
+            this.btndepositar.Size = new System.Drawing.Size(120, 43);
+            this.btndepositar.TabIndex = 14;
+            this.btndepositar.Text = "Depositar";
+            this.btndepositar.UseVisualStyleBackColor = true;
+            this.btndepositar.Click += new System.EventHandler(this.btndepositar_Click);
+            // 
+            // btnexibir
+            // 
+            this.btnexibir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexibir.Location = new System.Drawing.Point(59, 359);
+            this.btnexibir.Name = "btnexibir";
+            this.btnexibir.Size = new System.Drawing.Size(148, 43);
+            this.btnexibir.TabIndex = 15;
+            this.btnexibir.Text = "Exibir Saldo";
+            this.btnexibir.UseVisualStyleBackColor = true;
+            this.btnexibir.Click += new System.EventHandler(this.btnexibir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(767, 551);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btncriar);
+            this.Controls.Add(this.txtsaldo);
+            this.Controls.Add(this.txtconta);
+            this.Controls.Add(this.txtitular);
+            this.Controls.Add(this.txtagencia);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtagencia;
+        private System.Windows.Forms.TextBox txtitular;
+        private System.Windows.Forms.TextBox txtconta;
+        private System.Windows.Forms.TextBox txtsaldo;
+        private System.Windows.Forms.Button btncriar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnexibir;
+        private System.Windows.Forms.Button btndepositar;
+        private System.Windows.Forms.Button btnsacar;
+        private System.Windows.Forms.TextBox txtvalor;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ContaCorrente cc = new ContaCorrente();
+
+        private void btncriar_Click(object sender, EventArgs e)
+        {
+            cc.Agencia = txtagencia.Text;
+            cc.Agencia = txtconta.Text;
+            cc.Titular = txtitular.Text;
+            cc.Saldo = Convert.ToDouble(txtsaldo.Text);
+            cc.Limite = 500;
+            MessageBox.Show("Conta criada com sucesso!!");
+        }
+
+        private void btndepositar_Click(object sender, EventArgs e)
+        {
+            cc.Depositar(Convert.ToDouble(txtvalor.Text));
+            txtsaldo.Text = cc.Saldo.ToString();
+            txtvalor.Text = null;
+        }
+
+        private void btnsacar_Click(object sender, EventArgs e)
+        {
+            cc.Sacar(Convert.ToDouble(txtvalor.Text));
+        }
+
+        private void btnexibir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Titular: " + cc.Titular + "\nAgência: " + cc.Agencia + "\nNúmero da Conta: " +
+                cc.Numero + "\nSaldo: " + cc.Saldo.ToString("C") + "\nValor do júros: " +
+                cc.Calcular().ToString("C"));
+        }
+    }
+}
